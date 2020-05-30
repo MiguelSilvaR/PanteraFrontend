7ddbf87955732af7f084bc86aca5749a6565d95d
Miguel Silva Ramos <miguelsilva_ramos15@live.com.mx>
2020-05-30 12:58:23 -0500

    Nuevo proyecto tras dos versiones obsoletas

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,20 +16,20 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PanteraFrontend'`, () => {
+  it(`should have as title 'Panthera'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('PanteraFrontend');
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('Panthera');
   });
 
-  it('should render title', () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('PanteraFrontend app is running!');
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Panthera!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Shared/Navbar/navbar.component';
+import { FooterComponent } from './Shared/footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ApartadoComponent } from './apartado/apartado.component';
+import { ApartarNuevoComponent } from './apartar-nuevo/apartar-nuevo.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    DashboardComponent,
+    ApartadoComponent,
+    ApartarNuevoComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory })
   ],
   providers: [],
   bootstrap: [AppComponent]
